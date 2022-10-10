047fad62aabf249c6f281374e833b58f762a8600
Matheus Der <matheusder1@hotmail.com>
2022-10-10 15:28:04 -0300

    (api): configure in memory database

--- RoomBookingApp.Api/Program.cs
+++ RoomBookingApp.Api/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using RoomBookingApp.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var conn = new SqliteConnection(builder.Configuration.GetConnectionString("Sqlite"));
+conn.Open();
+
+builder.Services.AddDbContext<RoomBookingAppDbContext>(opt => opt.UseSqlite(conn));
 
 var app = builder.Build();
 
